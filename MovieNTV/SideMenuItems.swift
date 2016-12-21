e904c3075b52572d64c2d816ddc7e449356a28c2
Ken Siu <kensiu20042000@gmail.com>
2016-12-21 14:12:05 +0800

    put the sideMenu data in a model file

---
+++ MovieNTV/SideMenuItems.swift
@@ -0,0 +1,14 @@
+//
+//  SideMenuItems.swift
+//  MovieNTV
+//
+//  Created by Ken Siu on 21/12/2016.
+//  Copyright © 2016 Ken Siu. All rights reserved.
+//
+
+import Foundation
+
+class SideMenuItems {
+    static let menuItems = [["Now Playing", "Popular", "Top Rated", "Upcoming"],["Airing Today"]]
+    static let headerItems = ["Movies", "TV Shows"]
+}
