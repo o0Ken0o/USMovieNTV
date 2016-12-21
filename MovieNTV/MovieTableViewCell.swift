b11418f145a3738d746c85f25e6a0c41929118f5
Ken Siu <kensiu20042000@gmail.com>
2016-12-21 16:12:28 +0800

    add a switching to tableView function.

---
+++ MovieNTV/MovieTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieTableViewCell.swift
+//  MovieNTV
+//
+//  Created by Ken Siu on 21/12/2016.
+//  Copyright © 2016 Ken Siu. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
