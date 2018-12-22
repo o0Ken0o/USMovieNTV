903a080f958e134414df8bf956ff1e423f4bf2d7
Ken Siu <kensiu20042000@gmail.com>
2018-12-22 22:32:32 +0800

    create different coordinators

---
+++ MovieNTV/TVs/TVsCoordinator.swift
@@ -0,0 +1,23 @@
+//
+//  TVsCoordinator.swift
+//  MovieNTV
+//
+//  Created by Kam Hei Siu on 22/12/2018.
+//  Copyright © 2018 Ken Siu. All rights reserved.
+//
+
+import UIKit
+
+class TVsCoordinator: BaseCoordinator {
+    private let presenter: UINavigationController
+    private let tabBarTag: Int
+    
+    init(presenter: UINavigationController, tabBarTag:Int) {
+        self.presenter = presenter
+        self.tabBarTag = tabBarTag
+    }
+    
+    func start() {
+        
+    }
+}
